2674ed88e5ba780cab7aa3ffe0e3883d79b70adc
brk <kbridge@126.com>
2015-10-16 00:23:23 +0800

    undone. used native type names.
    to NoviceLive: this is not the right place to change output type names.

--- PdbReader/PrimTypes.cs
+++ PdbReader/PrimTypes.cs
@@ -8,23 +8,23 @@ namespace PdbReader
 {
     static class PrimTypes
     {
-        public static CPrim VOID        = new CPrim("VOID");
+        public static CPrim VOID        = new CPrim("void");
 
-        public static CPrim WCHAR       = new CPrim("WCHAR");
+        public static CPrim WCHAR       = new CPrim("wchar_t");
 
-        public static CPrim CHAR        = new CPrim("CHAR");
-        public static CPrim INT         = new CPrim("INT");
-        public static CPrim SHORT       = new CPrim("SHORT");
-        public static CPrim LONG        = new CPrim("LONG");
-        public static CPrim LONGLONG    = new CPrim("LONGLONG");
+        public static CPrim CHAR        = new CPrim("char");
+        public static CPrim INT         = new CPrim("int");
+        public static CPrim SHORT       = new CPrim("short");
+        public static CPrim LONG        = new CPrim("long");
+        public static CPrim LONGLONG    = new CPrim("__int64");
 
-        public static CPrim UCHAR       = new CPrim("UCHAR");
-        public static CPrim UINT        = new CPrim("UINT");
-        public static CPrim USHORT      = new CPrim("USHORT");
-        public static CPrim ULONG       = new CPrim("ULONG");
-        public static CPrim ULONGLONG   = new CPrim("ULONGLONG");
+        public static CPrim UCHAR       = new CPrim("unsigned char");
+        public static CPrim UINT        = new CPrim("unsigned int");
+        public static CPrim USHORT      = new CPrim("unsigned short");
+        public static CPrim ULONG       = new CPrim("unsigned long");
+        public static CPrim ULONGLONG   = new CPrim("unsigned __int64");
 
-        public static CPrim FLOAT       = new CPrim("FLOAT");
-        public static CPrim DOUBLE      = new CPrim("DOUBLE");
+        public static CPrim FLOAT       = new CPrim("float");
+        public static CPrim DOUBLE      = new CPrim("double");
     }
 }
