1763f0f92dd7fea0f533c20572807d6f16a4dce3
brk <kbridge@126.com>
2015-10-29 23:36:56 +0800

    written py.XmlParser and py.TopoSorter

--- Support/Python/pdbxml.py
+++ Support/Python/pdbxml.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as et
+#====================================================================#
 def maybeSpace(s):
     return ' ' + s if s else ''
 
@@ -204,6 +206,14 @@ class TypeAttrs(object):
     const     = TypeAttr('const', -2)
     volatile  = TypeAttr('volatile', -1)
 
+    __map = {}
+    for attr in [unaligned, const, volatile]:
+        __map[attr.name] = attr
+
+    @classmethod
+    def lookup(cls, s):
+        return cls.__map[s]
+
 class CAttrTerm(CTerm):
     def __init__(self, type, attrs):
         self.__type = type
@@ -228,7 +238,165 @@ class CBits(CWrap):
     
     # (impl)
     def decorate(self, s):
-        return s + ' : ' + str(self.len);
+        return s + ' : ' + str(self.len)
+
+class PrimTypes(object):
+    VOID        = CPrim('void')
+    WCHAR       = CPrim('wchar_t')
+    
+    CHAR        = CPrim('char')
+    INT         = CPrim('int')
+    SHORT       = CPrim('short')
+    LONG        = CPrim('long')
+    LONGLONG    = CPrim('__int64')
+    
+    UCHAR       = CPrim('unsigned char')
+    UINT        = CPrim('unsigned int')
+    USHORT      = CPrim('unsigned short')
+    ULONG       = CPrim('unsigned long')
+    ULONGLONG   = CPrim('unsigned __int64')
+
+    FLOAT       = CPrim('float')
+    DOUBLE      = CPrim('double')
 
 #====================================================================#
+class Locator(object):
+    def __init__(self, elem):
+        self.__elem = elem
+        self.__index = -1
 
+    def find(self, id):
+        self.__index += 1
+        child = self.__elem[self.__index]
+        print 'cur id = ', child.attrib['id']
+        print 'expected id =', id
+        assert int(child.attrib['id']) == id
+        return child
+
+class XmlParser(object):
+    def __init__(self, path):
+        tree = et.parse(path)
+        root = tree.getroot()
+        self.root = root
+        self.__locFunc = Locator(root[0])
+        self.__locUnnamed = Locator(root[1])
+        
+    def readNamed(self, eType):
+        return self.readGroup(eType)
+
+    def readUnnamed(self, id):
+        eUnnamed = self.__locUnnamed.find(id)
+        return self.readGroup(eUnnamed)
+
+    def readGroup(self, eType):
+        if eType.tag == 'enum':
+            return self.readEnum(eType)
+        else:
+            tree = CStruct() if eType.tag == 'struct' else CUnion()
+            return self.readTree(eType, tree)
+
+    def readTree(self, eType, t):
+        for eField in eType:
+            name = eField.attrib['name']
+            # TODO? offset
+            # TODO? merge into readField()
+            typ = self.readField(eField)
+            t.add(typ, name)
+        return t
+
+    def readEnum(self, eType):
+        t = CEnum()
+        for eConst in eType:
+            name = eConst.attrib['name']
+            val = eConst.attrib['val']
+            t.add(name, val)
+        return t
+    
+    def readField(self, eField):
+        if eField.tag == 'bit-field':
+            t = self.readType(eField, False)
+            t = CBits(t, int(eField.attrib['len']))
+        else:
+            t = self.readType(eField, True)
+        return t
+    
+    def readType(self, eField, hasWrap):
+        attrs = eField.attrib
+        t = self.readBase(attrs['base'])
+        if attrs.has_key('attr'):
+            t = CAttrTerm(t, self.readAttr(attrs['attr']))
+        if hasWrap:
+            t = self.readWrap(attrs['wrap'], t)
+        return t
+    
+    def readAttr(self, attr):
+        attrSet = set()
+        for s in attr.split():
+            attrSet.add(TypeAttrs.lookup(s))
+        return attrSet
+    
+    def readBase(self, base):
+        sigil = base[0]
+        name = base[1:]
+        if sigil == '.':
+            return self.readPrim(name)
+        elif sigil == '$':
+            return self.readTypeRef(name)
+        else:
+            return self.readUnnamed(int(name))
+    
+    # TODO define this as a class var
+    __primTypes = {
+        'VOID':         PrimTypes.VOID,
+        'WCHAR':        PrimTypes.WCHAR,
+        
+        'CHAR':         PrimTypes.CHAR,
+        'UCHAR':        PrimTypes.UCHAR,
+        'SHORT':        PrimTypes.SHORT,
+        'USHORT':       PrimTypes.USHORT,
+        'INT':          PrimTypes.INT,
+        'UINT':         PrimTypes.UINT,
+        'LONG':         PrimTypes.LONG,
+        'ULONG':        PrimTypes.ULONG,
+        
+        'LONGLONG':     PrimTypes.LONGLONG,
+        'ULONGLONG':    PrimTypes.ULONGLONG,
+        
+        'FLOAT':        PrimTypes.FLOAT,
+        'DOUBLE':       PrimTypes.DOUBLE,
+    }
+    
+    def readPrim(self, name):
+        return XmlParser.__primTypes[name]
+    
+    def readTypeRef(self, name):
+        return CTypeRef(name)
+    
+    def readWrap(self, wrap, t):
+        arr = wrap.split()
+        i = 0
+        while i < len(arr):
+            c = arr[i].lower()
+            if c == 'a':
+                t = CArr(t, int(arr[i + 1]))
+                i += 2
+            elif c == 'p':
+                t = CPtr(t)
+                i += 1
+            else:
+                t = self.readFunc(int(arr[i + 1]), t)
+                i += 2
+        return t
+
+    def readFunc(self, id, t):
+        eFunc = self.__locFunc.find(id)
+        t = CFunc(t)
+        for eArg in eFunc:
+            t.add(self.readType(eArg, True))
+        return t
+
+
+parser = XmlParser('f:\\ntkrnlmp.xml')
+named = parser.root[2]
+
+print parser.readGroup(named[5]).define('x', '....', '    ')
